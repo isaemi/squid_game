89a0c08b482efc42111e717ffc006b5a659c7782
Saemi Park <imsam0124@gmail.com>
2024-01-05 11:16:13 -0800

    added animation

--- projectV1.js
+++ projectV1.js
@@ -1,7 +1,7 @@
 function Drawbots() {
     let bots = document.querySelector("#bots");
     let computer = `<circle class="anemiesalive" cx="500" cy="250" r="5" fill="black" />`;
-    for (i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++){
         computer += `<circle class="anemiesalive" cx="500" cy="${Math.random() * 500}" r="5" fill="black" />`;
     }
     bots.innerHTML = computer;
@@ -23,7 +23,8 @@ function processGame(e){
     let bots = document.querySelectorAll(".anemiesalive");
     let output = document.querySelector("#output");
 
-    if(player.getAttribute("class") == "player") {
+    // Check if the player's class contains "player"
+    if(player.getAttribute("class").includes("player")) {
         let cx = Number(player.getAttribute("cx"));
         let cy = Number(player.getAttribute("cy"));
         let mouseXposition = e.clientX - 10;
@@ -31,39 +32,44 @@ function processGame(e){
 
         player.setAttribute("cx", mouseXposition);
         player.setAttribute("cy", mouseYposition);
+        player.classList.add("animated-svg");
 
-        if((cx - mouseXposition) > 37) {
+        if((cx - mouseXposition) > 40) {
             player.setAttribute("fill", "red");
             player.setAttribute("class", "playerdead");
             output.innerHTML = "You Were Shot";
         }
 
-        for( i = 0; i < bots.length; i++) {
+        for(let i = 0; i < bots.length; i++) {
             let x = Number(bots[i].getAttribute("cx"));
             let y = Number(bots[i].getAttribute("cy"));
-            let randomX = (Math.random() * 40);
-            let randomY = (Math.random() * 40) - 20;
+            let randomX = (Math.random() * 43);
+            let randomY = (Math.random() * 43) - 20;
             bots[i].setAttribute("cx", x - randomX)
             bots[i].setAttribute("cy", y - randomY)
 
-            if((randomX) > 37){
+            bots[i].classList.add("animated-svg");
+
+            if((randomX) > 40){
                 bots[i].setAttribute("fill", "red");
                 bots[i].setAttribute("class", "anemiesdead");
             }
         }
-    }
-    if(player.getAttribute("cx") < 100 && player.getAttribute("class") == "player"){
-        player.setAttribute("class", "playerdead");
-        player.setAttribute("fill", "green");
-        output.innerHTML = "You Win";
-    }
-    for (i = 0; i < bots.length; i++){
-        if(bots[i].getAttribute("cx") < 100){
+
+        if(player.getAttribute("cx") < 100 && player.getAttribute("class").includes("player")){
             player.setAttribute("class", "playerdead");
-            bots[i].setAttribute("fill", "green");
-            i = bots.length;
-            output.innerHTML = "You Lost";
-            break;
+            player.setAttribute("fill", "green");
+            output.innerHTML = "You Win";
         }
-    }
+
+        for (let i = 0; i < bots.length; i++){
+            if(bots[i].getAttribute("cx") < 100){
+                player.setAttribute("class", "playerdead");
+                bots[i].setAttribute("fill", "green");
+                i = bots.length;
+                output.innerHTML = "You Lost";
+                break;
+            }
+        }
+    }    
 }
